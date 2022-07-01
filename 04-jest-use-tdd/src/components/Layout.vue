6c28dc2323f1cc92658743167703a037b760e179
yellowsea <yunhai0644@163.com>
2022-07-01 16:05:07 +0800

    chore: update

---
+++ 04-jest-use-tdd/src/components/Layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- 默认插槽 -->
+    <slot></slot>
+    <header>
+      <!-- 具名插槽 -->
+      <slot :msg="msg" name="header"></slot>
+    </header>
+    <main>
+      <!-- 具名插槽 -->
+      <slot name="main"></slot>
+    </main>
+    <footer>
+      <!-- 具名插槽 -->
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+      msg: "message",
+    }
+  }
+}
+</script>
